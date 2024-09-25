543b63235cc1ad336c5ed00f744a625c5794ec01
infocareerdemo <137291933+infocareerdemo@users.noreply.github.com>
2024-09-25 12:17:15 +0530

    Delete QA-InfoCareer-KCS-REST-ASSURED directory

--- QA-InfoCareer-KCS-REST-ASSURED/src/main/java/com/catering/enumeration/Authors.java
+++
@@ -1,7 +0,0 @@
-package com.catering.enumeration;
-
-public enum Authors {
-	
-	USER_1, 
-	USER_2
-}
--- QA-InfoCareer-KCS-REST-ASSURED/src/main/java/com/catering/enumeration/CategoryType.java
+++
@@ -1,9 +0,0 @@
-package com.catering.enumeration;
-
-public enum CategoryType {
-
-	REGRESSION,
-	Black_Box_Testing,
-	SMOKE,
-	SANITY
-}
